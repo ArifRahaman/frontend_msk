4047ff243387c27f29e0a20230448459de5aca45
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2026-03-21 13:51:50 +0000

    refactor: remove commented-out dead code in main.jsx

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
